143046a3758fc442491316acb47082eb594d111a
=Bharathxworkz <=bharathllakshmanm@gmail.com>
2024-08-09 15:02:37 +0530

    inheritance

--- Hero.java
+++
@@ -1,7 +0,0 @@
-class Hero
-{
-public static void main(String[] hhf)
-{
-System.out.println("ok");
-}
-}
--- bank-system/src/com/xworkz/bankapp/currentaccount/CurrentAccount.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.bankapp.currentaccount;
-import com.xworkz.bankapp.bankaccount.bankaccount;
-public class CurrentAccount extends bankaccount {
-
-    public void setBalance(double minbalance) {
-        setBalance(minbalance);
-    }
-
-}
